4b8397443b5f90375b9ece64898127213980b8dd
Hung-Ming Wu <whungming@yahoo.com>
2019-07-26 16:44:17 +0800

    compute averagereviews

--- reviews_analystics.py
+++ reviews_analystics.py
@@ -6,7 +6,10 @@ with open('reviews.txt', 'r') as f: #as 當作
 		count += 1
 		if count % 1000 == 0:
 		    print(len(data))
-print(len(data))
-print(data[0])
-print('----------------')
-print(data[1])
+print('檔案讀完了, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print('留言的平均長度', sum_len / len(data))
